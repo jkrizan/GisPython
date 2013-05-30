0c0e2ed373849d72b3087b08efdd3522e8f6d7fb
Josip Križan <josip.krizan@gmail.com>
2013-05-30 23:47:43 +0200

    Added few comments, buffer_distance, simplify_tolerance and dissolve_attrib

--- dissolve.py
+++ dissolve.py
@@ -13,59 +13,6 @@ import time
 import numpy as np
 import sys
 
-def dissolve1():
-    
-    inshape=r'E:\WORK\RAZNO\lpis\lpis_inicijalni.shp'
-    codes=[210,400,424,900,421,422,420,320,310,490,410,500,300,200]
-    outshape=r'E:\WORK\RAZNO\lpis\lpis_diss_cus.shp'
-    
-    c = fiona.collection(inshape, "r")
-    driver=c.driver
-    crs=c.crs
-    geom=[]
-    value=[]
-    zup=[]
-    
-    print 'Reading ...'
-    for i,f in enumerate(c):
-        if i%1000 == 0:
-            print i
-        geom.append(shape(f['geometry']).simplify(1.0))
-        value.append(f['properties']['KOR_ZEM'])
-        zup.append(f['properties']['ZUPANIJA'])
-    print 'Done.'
-    
-    geom = np.array(geom)
-    value=np.array(value)
-    zup=np.array(zup)
-    zupu=np.unique(zup)
-    
-    i=0
-    rec={'geometry':{'coordinates':None,'type':'Polygon'},'properties':{'kor_zem':None}}
-    schema={'geometry':'Polygon','properties':{'kor_zem':'int'}}
-    with fiona.open(outshape,'w',driver,schema,crs) as out:   
-        for code in codes:
-            for z in zupu:
-                print code #, z
-                sub_c=geom[(value==code)&(zup==z)]#[shape(f['geometry']) for f in c if f['KOR_ZEM']==code]
-                print ' have {} records ...'.format(len(sub_c))
-                ta = time.time()
-                u = cascaded_union(sub_c)
-                print 'Union in {} secs'.format(time.time()-ta)
-                if u.type=='Polygon':
-                    u=[u]
-                for f in u:
-                    i+=1
-                    if i%1000==0:
-                        print 'Writing record {}'.format(i)
-                    #gg=[[coord for coord in f.exterior.coords]]
-                    #for ii in f.interiors:
-                    #    gg.append([coord for coord in ii.coords])
-                    gg=mapping(f)
-                    rec['geometry']['coordinates']=gg
-                    rec['properties']['kor_zem']=code
-                    out.write(rec)
-                    
 def read_from_cover(filename,bbox):
     c = fiona.open(filename, "r")
     geom=[]; value=[]
@@ -79,12 +26,21 @@ def read_from_cover(filename,bbox):
     print 'Done.'
     return np.array(geom),np.array(value)
     
-def dissolve2(inshape,outshape,nx=5,ny=5):
-     
+def dissolve(inshape,dissolve_attrib,outshape,nx=5,ny=5,buffer_distance=0,simplify_tolerance=0):
+    """
+    Dissolves inshape with respect to dissolve_attrib and saves output to outshape.
+    Algorithm is designed for very large number of polygons (verified on 800000).
+    nx, ny -- number of divisions on x and y axes
+    buffer_distance -- distance of buffer applied to collection of polygons
+    simplify_tolerance -- tolerance for simplify function applied on unioned polygons
+                    before saving to output shape
+    """
+    # read input vector
     c = fiona.open(inshape, "r")
     driver=c.driver
     crs=c.crs
     
+    # calculate covers of entire shape
     covers=[]
     dx=(c.bounds[2]-c.bounds[0])/nx
     dy=(c.bounds[3]-c.bounds[1])/ny
@@ -95,55 +51,59 @@ def dissolve2(inshape,outshape,nx=5,ny=5):
             covers.append(bbox)
     c.close()
     
-    inters_geom=[]
-    inters_code=[]
+    inters_geom=[]  # geometries which intersects boundary of covers
+    inters_code=[]  # codes of that geometries
     i=0
-    rec={'geometry':{'coordinates':None,'type':'Polygon'},'properties':{'kor_zem':None}}
-    schema={'geometry':'Polygon','properties':{'kor_zem':'int'}}
+    rec={'geometry':{'coordinates':None,'type':'Polygon'},'properties':{dissolve_attrib:None}}
+    schema={'geometry':'Polygon','properties':{dissolve_attrib:'int'}}
     with fiona.open(outshape,'w',driver,schema,crs) as out:
         for ci,cover in enumerate(covers):
             geom,value=read_from_cover(inshape,cover.bounds)
             print 'cover {}. {} shapes'.format(ci,len(geom))
             codes=np.unique(value)
             for code in codes:
-                sub_c=geom[(value==code)]#[shape(f['geometry']) for f in c if f['KOR_ZEM']==code]
+                sub_c=geom[(value==code)]
                 print '  code {} have {} records ...'.format(code,len(sub_c))
-                sys.stdout.flush()
-                ta = time.time()
+                #sys.stdout.flush()
+                #ta = time.time()
                 sub_c=shapely.geometry.MultiPolygon(sub_c) #u = shapely.ops.unary_union(sub_c)#cascaded_union(sub_c)
-                u=sub_c.buffer(0.01)
-                print '  union in {} secs'.format(time.time()-ta)
-                if u.type=='Polygon':
+                u=sub_c.buffer(buffer_distance)
+                #print '  union in {} secs'.format(time.time()-ta)
+                if u.type=='Polygon': #Only one polygon
                     u=[u]
                 for f in u:
-                    if f.intersects(cover.boundary):    # Ako sijeće ....
+                    if f.intersects(cover.boundary):    # If polygon intersetcs cover boundary
                         inters_geom.append(f)
                         inters_code.append(code)
                     else:          
                         i+=1
-                        if i%100==0:
+                        if i%1000==0:
                             print 'Writing record {}'.format(i)
-                        gg=mapping(f.simplify(0.1))
+                        if simplify_tolerance>0.0:
+                            f=f.simplify(simplify_tolerance)
+                        gg=mapping(f)
                         rec['geometry']=gg
-                        rec['properties']['kor_zem']=int(code)
+                        rec['properties'][dissolve_attrib]=int(code)
                         out.write(rec)   
         print '{} polygons on intersections'.format(len(inters_geom))
         geom=np.array(inters_geom)
         value=np.array(inters_code)
         codes=np.unique(value)
         for code in codes:
-            sub_c=geom[(value==code)]#[shape(f['geometry']) for f in c if f['KOR_ZEM']==code]
+            sub_c=geom[(value==code)]
             print '  code {} have {} records ...'.format(code,len(sub_c))
-            ta = time.time()
-            u=shapely.geometry.MultiPolygon(sub_c).buffer(0.01)#u = cascaded_union(sub_c)
-            print '  union in {} secs'.format(time.time()-ta)
+            #ta = time.time()
+            u=shapely.geometry.MultiPolygon(sub_c).buffer(buffer_distance) #u = cascaded_union(sub_c)
+            #print '  union in {} secs'.format(time.time()-ta)
             if u.type=='Polygon':
                 u=[u]
             for f in u:      
                 i+=1
                 if i%1000==0:
                     print 'Writing record {}'.format(i)
-                gg=mapping(f.simplify(0.1))
+                if simplify_tolerance>0.0:
+                    f=f.simplify(simplify_tolerance)
+                gg=mapping(f)
                 rec['geometry']=gg
                 rec['properties']['kor_zem']=int(code)
                 out.write(rec)
@@ -152,7 +112,7 @@ if __name__=='__main__':
 
     inshape=r'E:\WORK\RAZNO\lpis\lpis_inicijalni.shp'
     outshape=r'E:\WORK\RAZNO\lpis\lpis_diss_3.shp'
-    dissolve2(inshape,outshape,nx=10,ny=10)
+    dissolve(inshape,outshape,nx=10,ny=10)
 	#nx=5
     #ny=5
 
